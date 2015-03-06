2a591142de41eede392d13cfe8de35f32500a5f1
Danny Rademacher <danny.rademacher@gmail.com>
2015-03-06 22:27:26 +0000

    debug only source

--- src/mapgen.py
+++ src/mapgen.py
@@ -1,4 +1,4 @@
-from random import randint, sample
+from random import randint, choice
 
 R = lambda x: int(round(x)) # wrapper for rounding
 
@@ -13,14 +13,14 @@ class GenerateMap(object):
 	def split(self, xlo, xhi, ylo, yhi):
 		# absolutely failed
 		if min(xhi-xlo, yhi-ylo) < 3:
-			return None
+			pass
 			
 		# generate room in splitted space
 		if (xhi-xlo) * (yhi-ylo) <= 30:
 			# random split
-			values = sample(range(xlo, xhi), 3) # 3 adjust size
+			values = [randint(xlo, xhi) for _ in range(3)] # sample(range(xlo, xhi), 3)
 			xlo, xhi = min(values), max(values)
-			values = sample(range(ylo, yhi), 3) # 3 adjust size
+			values = [randint(ylo, yhi) for _ in range(3)] # sample(range(ylo, yhi), 3)
 			ylo, yhi = min(values), max(values)
 
 			# set room
@@ -46,7 +46,23 @@ class GenerateMap(object):
 				return a
 
 			# a, b yield rooms
-
+			# if a[3] > b[3]:
+			# 	a,b = b,a
+			rangea = set(range(a[0], a[1]+1))
+			rangeb = set(range(b[0], b[1]+1))
+			
+			poss = rangea.intersection(rangeb)
+			if poss:
+				x = choice(poss)
+				ylo = a[3]
+				yhi = b[2]
+				self.set_room(x, x, ylo, yhi)
+			else:
+				print a, rangea
+				print b, rangeb
+				print "this means work"
+			
+				
 
 
 
