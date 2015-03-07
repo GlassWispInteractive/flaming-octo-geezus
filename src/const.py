ade8a6e4efeedab452151322d06c3a8b3f538765
Lennart Buhl <git@hackmate.de>
2015-03-07 20:03:39 +0100

    random placement of player

--- src/const.py
+++ src/const.py
@@ -4,8 +4,8 @@
 ## GLOBAL CONSTANTS
 X = 960
 Y = 640
-DUNGEON_X = 20
-DUNGEON_Y = 50
+DUNGEON_X = 30
+DUNGEON_Y = 40
 FPS = 15
 SCALE = 32 ## Only Change for Testing
 ## Standard: 32
--- src/player.py
+++ src/player.py
@@ -16,8 +16,12 @@ class Player(object):
 		if pos != None:
 			cls.x, cls.y = pos
 		else:
-			m = cls.cur_dungeon().level
-			x, y = random.sample()
+			m = cls.W.cur_dungeon()
+			x, y = 0, 0
+			while m.level[x][y] not in list(range(1,3)):
+				x, y = random.randint(0, m.size_x), random.randint(0, m.size_y)
+			cls.x, cls.y = x, y
+		print 'spawn player @', (cls.x, cls.y)
 
 		cls.commands = [] # list of commands to execute; EventHandler writes to this
 		cls.sprite = sprites.CharSetMultiSprite("graphics/Chara1.png", 24,32, 4,0, 72,128)
@@ -38,4 +42,4 @@ class Player(object):
 
 		def try_pos(x,y):
 			if cls.W.cur_dungeon().level[x][y] != 0: # if movement is allowed
-				self.x, self.y = x, y # then move
+				cls.x, cls.y = x, y # then move
