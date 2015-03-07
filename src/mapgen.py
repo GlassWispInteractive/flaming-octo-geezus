26550f3e38f9fa1b0b72af52220f8a5e1015edd1
Danny Rademacher <danny.rademacher@gmail.com>
2015-03-07 16:00:03 +0100

    TETRIS LOVE

--- src/mapgen.py
+++ src/mapgen.py
@@ -2,11 +2,12 @@ from random import randint, sample, choice
 
 R = lambda x: int(round(x)) # wrapper for rounding
 
+
 class GenerateMap(object):
 	"""class which is responsible for the world generation"""
-	def __init__(self):
+	def __init__(self, x, y):
 		super(GenerateMap, self).__init__()
-		self.x, self.y = (100, 60)
+		self.x, self.y = (x, y)
 		self.map = [[0]*self.y for i in range(self.x)]
 		self.split(0, self.x, 0, self.y, 4)
 
@@ -92,6 +93,79 @@ class GenerateMap(object):
 				self.map[xlo+x][ylo+y] = 1
 
 
+
+
+
+class GenerateMapTetris(object):
+	"""class which is responsible for the world generation"""
+	def __init__(self, x, y):
+		super(GenerateMap, self).__init__()
+		self.x, self.y = (x, y)
+		self.map = [[0]*self.y for i in range(self.x)]
+		# self.gen_rooms(0, self.x, 0, self.y, 4)
+		self.gen_rooms(4, 7, 5)
+
+	def gen_rooms(self, lo, hi, count):
+		room = self.rand_room(lo, hi)
+		xoff, yoff = R((self.x-room[0])/2), R((self.y-room[1])/2)
+		self.set_room(xoff, xoff + room[0], yoff, yoff + room[1])
+
+		for _ in range(count): # room
+			room = self.rand_room(lo, hi)
+
+			for _1 in sample(range(4), 4):
+				# rotate array
+				self.map = [list(t) for t in zip(*self.map[::-1])]
+				# print self.map
+
+				limit = len(self.map)
+
+				depth = [0] * limit
+				# check maximum depth
+				for z in range(limit):
+					for d in range(len(self.map[0])):
+						if self.map[z][d]:
+							break
+						depth[z] = d
+					# print depth[z]
+
+				# calc deepest size
+				val, ind = min(depth[:room[0]]), 0
+				for k in range(limit-room[0]+1):
+					if min(depth[k:k+room[0]]) > val:
+						ind = k
+
+				if depth > room[0]:
+					# print ind, ind + room[0], val - room[1], val
+					self.set_room(ind, ind + room[0], val - room[1], val)
+					break
+				# print ind, depth[ind]
+
+
+	def rand_room(self, lo, hi):
+		return randint(lo, hi), randint(lo, hi)
+
+
+	def set_room(self, xlo, xhi, ylo, yhi):
+		print xlo, xhi, ylo, yhi
+
+		for x in range(xhi-xlo):
+			for y in range(yhi-ylo):
+				self.map[xlo+x][ylo+y] = 1
+
+
+	def rotate(self, x, y, c):
+		"MAGIC MAGIC MAGIC MAGIC MAGIC MAGIC <3"
+		if c == 0:
+			return x,y
+		elif c == 1:
+			return self.x - y, x
+		elif c == 2:
+			return self.x - x, self.y - y
+		elif c == 3:
+			return y, self.y - x
+
+		
 if __name__ == '__main__':
 	import pprint;
 	gen = GenerateMap()
