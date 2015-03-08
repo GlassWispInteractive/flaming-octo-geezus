a168b3d4f895f7f2cb3584c14df3254c7519d0b1
Lennart Buhl <git@hackmate.de>
2015-03-08 18:15:46 +0100

    use Vanilla font in pink

--- src/menu.py
+++ src/menu.py
@@ -5,16 +5,14 @@ import pygame
 
 from const import *
 from helper import *
+from resources import *
 
 class Menu(object):
 
 	@classmethod
 	def init(cls):
 		cls.LOGO = pygame.image.load('graphics/logo.png').convert_alpha()
-		cls.FONT = pygame.font.SysFont("Consolas", 20)
-		cls.MENU_FONT = pygame.font.Font("fonts/starcraft.ttf", 22)
-		cls.items = ["Start game", "Highscore", "Exit"]
-		cls.current = 0
+		cls.MENU_FONT = pygame.font.Font("fonts/Vanilla.ttf", 26)
 
 	@classmethod
 	def draw_menu(cls, surf):
@@ -23,6 +21,6 @@ class Menu(object):
 		surf.blit(cls.LOGO, logorect)
 		pygame.draw.rect(surf, (192, 192, 192), (R(X*0.2778), R(Y*0.6), R(X*0.4444), R(Y*0.12)))
 		pygame.draw.rect(surf, (80, 80, 80), (R(X*0.2833), R(Y*0.61), R(X*0.4333), R(Y*0.1)))
-		label = cls.FONT.render("Press ENTER to start", 1, (200, 200, 200))
+		label = cls.MENU_FONT.render("Press ENTER to start", 1, DANNY_PINK)
 		pos = label.get_rect(centerx = R(X*0.5), centery = R(Y*0.66))
 		surf.blit(label, pos)
