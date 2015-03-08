ef2ce162b4ce6d170a6415f53e505ac57c70c1fc
Lennart Buhl <git@hackmate.de>
2015-03-08 15:57:46 +0100

    better starting

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Legacy ^^
-python run.py
--- src/resources.py
+++ src/resources.py
@@ -23,7 +23,7 @@ def init_resources():
 	global SOUNDS
 	global FONTS
 	GRAPHICS = { path.basename(path.splitext(s)[0])
-		: pygame.image.load(s).convert_alpha() for s in glob.glob("graphics/*.png") }
+		: pygame.image.load(s).convert_alpha() for s in glob.glob("fonts/*.png") }
 	SOUNDS = None
 	FONTS = {
 				'HUD' : pygame.font.Font("resources/pixel.ttf", 20)
